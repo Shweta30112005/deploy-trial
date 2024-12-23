c7ebda9442ab71aaad942bd240beb7badbef1fec
Shweta <b23cm1037@iitj.ac.in>
2024-12-23 12:41:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 style={{color:'#f1356d'}}>Deployment testing </h2>
+    </div>
+  );
+}
+
+export default App;
